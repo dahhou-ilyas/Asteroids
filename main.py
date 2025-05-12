b90b63023deb480464f938cd9f39a8a77012f8db
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-05-12 14:02:51 +0100

    optmz

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ def main():
         			return
 		screen.fill("black")
 		pygame.display.flip()
-		fp = clock.tick(60)
-		dt = fp/1000
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
