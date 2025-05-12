949f7ea1815d462168727dee6c6b586decad3c30
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-05-12 13:46:01 +0100

    fix errros

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pygame
 from constants import *
 
 def main():
+	pygame.init()
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+	fp = pygame.time.Clock()
+	dt = 0
 	while True :
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
