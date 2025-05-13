91839acff3999ca47827eb820e929cd8c35376dc
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-05-13 16:50:03 +0100

    move

--- main.py
+++ main.py
@@ -17,9 +17,22 @@ def main():
         			return
 		screen.fill("black")
 		player.draw(screen)
+		player.update(dt)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
 
+# speed : vitesse en pixels par seconde (ex. : 200 px/s)
+# dt : delta time, durée écoulée entre deux images (en secondes)
+
+# ➤ speed * dt représente la distance à parcourir pendant le temps écoulé entre deux images.
+# Cela permet un déplacement fluide, peu importe le nombre de FPS (images par seconde).
+
+# ➤ En simplifié : c’est le nombre de pixels à bouger pendant cette frame
+# pour respecter une vitesse constante dans le temps réel.
+
+# Exemple :
+# Si speed = 200 (pixels/seconde) et dt = 0.016 (≈ 60 FPS),
+# alors le déplacement sera 200 * 0.016 = 3.2 pixels sur cette frame.
