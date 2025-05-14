4459364add81cb24235dacedc4106f50bf0d36e2
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-05-14 08:36:19 +0100

    draw circle

---
+++ asteroid.py
@@ -0,0 +1,8 @@
+from circleshape import CircleShape
+import pygame
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen,"white",self.position,self.radius,2)
