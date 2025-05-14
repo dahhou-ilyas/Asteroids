64407dfef3dbc9ae1287c84c577b6fcaf0d2609f
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-05-14 12:28:31 +0100

    add astroid

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -9,13 +10,14 @@ def main():
   
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
 
 	Player.containers = (updatable, drawable)
-
-	
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
 
 	player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
-	
+	asteroidField = AsteroidField()
 
 	dt = 0
 	while True :
@@ -28,7 +30,6 @@ def main():
 		screen.fill("black")
 
 		for obj in drawable: 
-			print(obj)
 			obj.draw(screen)
 		
 		pygame.display.flip()
